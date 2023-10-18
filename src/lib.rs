87bd0aab10c5f81fd022fc4fcbaa624bc051b8f6
Nicolas Fränkel <nicolas@frankel.ch>
2023-10-18 17:53:57 +0200

    Add the equivalent code from the previous post

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use num_complex::Complex;
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -10,5 +12,16 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn rust_over_pyo3(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(compute, m)?)?;
     Ok(())
+}
+
+#[pyfunction]
+fn compute(command: &str, a: Complex<f64>, b: Complex<f64>) -> PyResult<Complex<f64>> {
+    match command {
+        "add" => Ok(a + b),
+        "sub" => Ok(a - b),
+        "mul" => Ok(a * b),
+        _ => Err(PyValueError::new_err("Unknown command")),
+    }
 }
